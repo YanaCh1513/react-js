efb34e3e11f35a8f76aad5cf1da15f8647f1c744
yana <yana@gmail.com>
2023-01-07 00:40:57 +0400

    Task 7. Redux middlewares. Redux-Persist. (add persist storage)

--- src/store/index.js
+++ src/store/index.js
@@ -6,21 +6,48 @@ import { messagesReducer } from "./messages/reducer";
 import { addBotMessageMiddleware } from "./middlewares/middlewares";
 import thunk from "redux-thunk";
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // localStorage
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const rootReducer = combineReducers({
+    profile: profileReducer,
+    chats: chatsReducer,
+    messages: messagesReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = createStore(
-    combineReducers({
-        profile: profileReducer,
-        chats: chatsReducer,
-        messages: messagesReducer
-    }),
+    persistedReducer,
     composeEnhancers(applyMiddleware(
         addBotMessageMiddleware,
         thunk
     ))
-    // window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    // window.__REDUX_DEVTOOLS_EXTENSION__() // need for redux devtools
 )
+
+export const persistor = persistStore(store)
+
+// WITHOUT REDUCE-PERSIST
+// export const store = createStore(
+//     combineReducers({
+//         profile: profileReducer,
+//         chats: chatsReducer,
+//         messages: messagesReducer
+//     }),
+//     composeEnhancers(applyMiddleware(
+//         addBotMessageMiddleware,
+//         thunk
+//     ))
+//     // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//     // window.__REDUX_DEVTOOLS_EXTENSION__() // need for redux devtools
+// )
 
 export const profileStore = createStore(profileReducer); // leave as legacy for test part of the code
 
